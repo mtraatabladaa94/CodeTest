baa115fb7af0cea1fc63f7629c8365540e20f4d9
Michael Traña <mtraatabladaa94@gmail.com>
2017-05-06 14:21:19 -0600

    Codigo de Pruebas de: C# y Animaciones CSS

---
+++ CSharpNET/Rx/ReactiveExtension/ReactiveConsole/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.IO;
+using System.Reactive.Concurrency;
+using System.Reactive.PlatformServices;
+
+namespace ReactiveConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = @"c:\mifichero.txt";
+            var observableLines = Observable.Create<string>(observer =>
+            {
+                using (var reader = new StreamReader(filePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        observer.OnNext(line);
+                    }
+                }
+
+                observer.OnCompleted();
+
+                return () => { };
+            });
+
+            observableLines.Subscribe(Imprimir);
+
+            observableLines.SubscribeOn(NewThreadScheduler.Default).Subscribe(line => Lineas += line);
+        }
+
+        static string Lineas = "";
+
+        static void Imprimir(string mensaje)
+        {
+
+        }
+
+        static void Imprimir2()
+        {
+        }
+
+
+    }
+}
---
+++ CSharpNET/Rx/ReactiveExtension/ReactiveExtension/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveExtension
+{
+    class Producto
+    {
+        public System.Reactive.Subjects.ReplaySubject<int> observador;
+
+        public Producto()
+        {
+            this.observador = new System.Reactive.Subjects.ReplaySubject<int>();
+        }
+    }
+}
