b75d7c8953d8bf19e6ae2eef1fda3c07532f7cb4
Michael Traña <mtraatabladaa94@gmail.com>
2017-05-21 20:13:59 -0600

    PruebaWPF

---
+++ CSharpNET/ExampleWPF/ExampleWPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExampleWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Saludar(string Nombre = "")
+        {
+            MessageBox.Show("Hola como estas" + (!String.IsNullOrWhiteSpace(Nombre) ? (" " + Nombre) : "") +"?", "Prueba WPF", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Saludar(TestTextBox.Text);
+        }
+    }
+}
---
+++ CodeTestVisualStudio/UsandoYield/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsandoYield
+{
+    class Program
+    {
+        public static IEnumerable<int> TestEnum()
+        {
+            for (var i = 0; i < 4; i++)
+            {
+                Console.Write("yield ");
+                yield return i;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entera con yield: ");
+            foreach (var i in TestEnum())
+            {
+                Console.WriteLine("{0} ", i);
+            }
+
+            Console.WriteLine("\nHasta 2 completa: ");
+            var lFullList = TestEnum().ToArray();
+            foreach (var i in lFullList)
+            {
+                Console.Write("{0} ", i);
+                if (i == 2) break;
+            }
+
+            Console.WriteLine("\nHasta 2 con yield: ");
+            foreach (var i in TestEnum())
+            {
+                Console.Write("{0} ", i);
+                if (i == 2) break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
